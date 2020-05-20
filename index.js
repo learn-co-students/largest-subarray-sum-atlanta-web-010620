477a1e165950711e827f4db6876464577c13d244
tylergreason <tyler.greason@protonmail.com>
2020-05-20 11:15:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-function sumAll(array){
-    let sum = 0; 
-    array.forEach(item => {
-        sum += item; 
-    })
-    // console.log(sum); 
-    return sum; 
-}
-
-function findLargestInArray(array){
-    // lazy for now, use binary search later 
-    array.sort((a,b) => a-b); 
-    return array[array.length-1]; 
-}
-
 function largestSubarraySum(array){
     let max = 0; 
     let s = 0; 
@@ -25,25 +10,3 @@ function largestSubarraySum(array){
     }
     return max; 
 }
-
-function recursion(a){
-    let sums = []; 
-    a.forEach(function(item, index){
-        // console.log(a);
-        // console.log(a.slice(0, a.length-(index+1)))
-        // console.log(a.slice(index))
-        let newArray = a.slice(index); 
-        if (a.length > 0){
-            recursion(a.slice(index+1));
-            recursion(a.slice(0,a.length-(index+1)))
-        }
-        sums.push(sumAll(newArray))
-    })
-    let result =  findLargestInArray(sums); 
-    // console.log(`greatest is ${result}`)
-    // console.log(sums); 
-    return sums; 
-}
-
-let a = [1,2,3,4]; 
-recursion(a); 
